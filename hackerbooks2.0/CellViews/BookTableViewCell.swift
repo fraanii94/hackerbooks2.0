8ee02a1b5183eee47d6de1b7dbb5cceab6bf8c90
Francisco Navarro <frn94@live.com>
2016-09-16 09:27:08 +0200

    Initial Commit

---
+++ hackerbooks2.0/CellViews/BookTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  BookTableViewCell.swift
+//  hackerbooks2.0
+//
+//  Created by fran on 15/9/16.
+//  Copyright © 2016 Francisco Navarro Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var coverImage: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
